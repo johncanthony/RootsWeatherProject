cdf75a76f7166ddfab5c772a18eaf4b84b3246c6
johncanthony <john.claud.anthony@gmail.com>
2024-01-11 07:45:42 -0600

    Added install requirements to setup.py. Added architecture image

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='RootsWeatherProject',
     version='1.0.0',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=['pytest', 'fastapi', 'uvicorn'],
     author='John Anthony',
     description='A package holding microservices to create video from compiled weather image data',
     url='https://github.com/your_username/RootsWeatherProject',
