60f92bd86b56e0b43c0f8d2bba1a715bcebff478
johncanthony <john.claud.anthony@gmail.com>
2024-01-15 02:06:36 -0600

    Removed unneccessary logging from the bootstrapping entrypoiny

--- ManagerAPI/server.py
+++ ManagerAPI/server.py
@@ -1,12 +1,5 @@
 import uvicorn
 from ManagerAPI import managerAPI
-import logging
-import sys
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler(sys.stdout))
-logger.info('Starting ManagerAPI')
 
 if __name__ == '__main__':
     config = uvicorn.Config(managerAPI, port=8000, log_level="info")
