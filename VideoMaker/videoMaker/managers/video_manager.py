58c38598bb0f3d2c5d40d538eb14f7dfd2a23b27
johncanthony <john.claud.anthony@gmail.com>
2024-01-17 03:26:05 -0600

    Added videoManagers; Filled out the videoMaker run() method; added new requirements to setup.py

---
+++ VideoMaker/videoMaker/managers/video_manager.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+import ffmpeg
+import os
+import logging as log
+
+
+@dataclass
+class VideoBase:
+
+    video_name: str = "output"
+    image_directory: str = f'/images/{video_name}'
+    input_glob: str = f'{image_directory}/*.jpg'
+    output_file: str = f'{image_directory}/{video_name}.mp4'
+    video_bitrate: str = "5000k"
+
+    def input_directory_exists(self):
+        return False if not os.path.exists(self.image_directory) else True
+
+    def input_directory_not_empty(self):
+        return False if len(os.listdir(self.image_directory)) == 0 else True
+
+
+'''
+Youtube Shorts video ffmpeg command example:
+ffmpeg -framerate 15 -pattern_type glob -i "*_GOES16-*-GEOCOLOR-*.jpg" -i space_walk_short.mp3 -map 0:v -map 1:a -c:v libx264 -c:a aac -b:a 192k -r 24 -vf "scale=w=1080:h=1920,setsar=1" -af "afade=t=out:st=280:d=3" output.mp4
+
+'''
+
+
+@dataclass
+class ShortsVideoManager(VideoBase):
+
+    framerate: int = 15
+    audiofile: str = ""
+    vcodec: str = "libx264"
+    acodec: str = "aac"
+    audio_bitrate: str = "192k"
+    filters: str = "scale=w=1080:h=1920,setsar=1"
+
+    def build(self):
+        log.info(f'[FFMPEG] Creating shorts video for job: {self.video_name}')
+        (
+            ffmpeg
+            .input(self.input_glob, pattern_type='glob', framerate=self.framerate)
+            .input(self.audiofile)
+            .filter(self.filters)
+            .output(self.output_file, vcodec=self.vcodec, acodec=self.acodec, audio_bitrate=self.audio_bitrate, video_bitrate=self.video_bitrate)
+            .run()
+        )
+
+
+'''
+Youtube video ffmpeg command example:
+ffmpeg -framerate 15 -pattern_type glob -i *_GOES16-*-GEOCOLOR-*.jpg -c:v libx264 -r 24 -vf scale=1920x1080 ./output.mp4
+'''
+
+
+@dataclass
+class VideoManager(VideoBase):
+
+    framerate: int = 15
+    vcodec: str = "libx264"
+    filters: str = "scale=w=1920:h=1080"
+
+    def build(self):
+        log.info(f'[FFMPEG] Creating video for job: {self.video_name}')
+        (
+            ffmpeg
+            .input(self.input_glob, pattern_type='glob', framerate=self.framerate)
+            .filter(self.filters)
+            .output(self.output_file, vcodec=self.vcodec, video_bitrate=self.video_bitrate)
+            .run()
+        )
--- VideoMaker/videoMaker/videomaker.py
+++ VideoMaker/videoMaker/videomaker.py
@@ -1,6 +1,7 @@
 from LogHandler.logHandler import LogHandler
 from JobHandler.jobHandler import JobHandler
 from ManagerAPI.api.models.managedJob import ManagedJobModel
+from VideoMaker.videoMaker.managers.video_manager import VideoBase, VideoManager, ShortsVideoManager
 
 from time import sleep
 import logging as log
@@ -9,6 +10,19 @@ import logging as log
 def create_video(job: ManagedJobModel):
 
     log.info(f'Creating video for job: {job.job_id}')
+    job_resolution = job.job_resolution.split("x")
+
+    if not VideoBase(video_name=str(job.job_id)).input_directory_exists() or not VideoBase(video_name=str(job.job_id)).input_directory_exists():
+        log.error(f'No images found for job: {job.job_id}')
+        job.job_status = "error"
+        job.job_error += f'No images found for job: {job.job_id} ,'
+        return
+
+    if job_resolution[0] == job_resolution[1]:
+        VideoManager(str(job.job_id)).build()
+    else:
+        ShortsVideoManager(str(job.job_id)).build()
+
     return
 
 
@@ -19,6 +33,22 @@ def run():
     if not jobHandler.available_job():
         log.debug('No grabbed jobs found')
         return
+
+    new_job = jobHandler.fetch_job()
+
+    if not new_job:
+        log.error(f'Failed to fetch job {new_job.job_id}')
+        return
+
+    create_video(new_job)
+
+    if new_job.job_status == "error":
+        jobHandler.update_job(new_job)
+        log.error(f'Updated job {new_job.job_id} with error')
+        return
+
+    new_job.job_status = "packed"
+    jobHandler.update_job(new_job)
 
     return
 
