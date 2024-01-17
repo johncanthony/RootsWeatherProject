4bf8d0d6d8e5463948072b12834673fa9578003e
johncanthony <john.claud.anthony@gmail.com>
2024-01-16 21:38:51 -0600

    Added uvicorn logging config

--- ManagerAPI/server.py
+++ ManagerAPI/server.py
@@ -2,6 +2,6 @@ import uvicorn
 from ManagerAPI import managerAPI
 
 if __name__ == '__main__':
-    config = uvicorn.Config(managerAPI, port=8000, log_level="info")
+    config = uvicorn.Config(managerAPI, port=8000, log_level="info", log_config="log.ini")
     server = uvicorn.Server(config)
     server.run()
