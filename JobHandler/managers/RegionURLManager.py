f6eee0ad167a9ce8a59cf76fb88ee984e98aa58c
johncanthony <john.claud.anthony@gmail.com>
2024-01-18 17:12:58 -0600

    Fixed RegionURLManager mapping for failing requests

--- JobHandler/managers/RegionURLManager.py
+++ JobHandler/managers/RegionURLManager.py
@@ -3,12 +3,12 @@ from dataclasses import dataclass
 
 @dataclass
 class RegionURLManager:
-    alaska: str = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/ak/GEOCOLOR/"
-    central_alaska: str = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/cak/GEOCOLOR/"
+    alaska: str = "https://cdn.star.nesdis.noaa.gov/GOES18/ABI/SECTOR/ak/GEOCOLOR/"
+    central_alaska: str = "https://cdn.star.nesdis.noaa.gov/GOES18/ABI/SECTOR/cak/GEOCOLOR/"
     south_mississippi_valley: str = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/smv/GEOCOLOR/"
     north_mississippi_valley: str = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/umv/GEOCOLOR/"
-    pac_west: str = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/wus/GEOCOLOR/"
-
+    pac_west: str = "https://cdn.star.nesdis.noaa.gov/GOES18/ABI/SECTOR/wus/GEOCOLOR/"
+    mexico: str = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/mex/GEOCOLOR/"
     conus: str = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/"
 
     def __getitem__(self, item):
