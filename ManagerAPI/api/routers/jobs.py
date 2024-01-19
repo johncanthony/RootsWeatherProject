0449046ca1f295e98f362f0006a04617a46a75f8
johncanthony <john.claud.anthony@gmail.com>
2024-01-19 01:06:25 -0600

    Added endpoints for job status, and fetching video file

--- ManagerAPI/api/routers/jobs.py
+++ ManagerAPI/api/routers/jobs.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import FileResponse
 from ManagerAPI.api.managers.job_manager import JobManager
 from ManagerAPI.api.models.managedJob import ManagedJobModel
 from ManagerAPI.api.managers.state_manager import StateManager
@@ -69,3 +70,35 @@ async def get_jobs_from_queue(queue):
         raise HTTPException(status_code=404, detail='No jobs in queue')
 
     return {queue: job_id}
+
+
+@jobRouter.get('/job/id/{job_id}/status', tags=['jobs'])
+async def get_job_status(job_id):
+
+    job = jobManager.get_job(job_id=job_id)
+
+    if job is None:
+        log.error(f'Job {job_id} not found')
+        raise HTTPException(status_code=404, detail='Job not found')
+
+    return {'status': job.job_status}
+
+
+@jobRouter.get('/job/id/{job_id}/video', tags=['jobs'])
+async def get_job_video(job_id):
+
+    job = jobManager.get_job(job_id=job_id)
+
+    if job is None:
+        log.error(f'Job {job_id} not found')
+        raise HTTPException(status_code=404, detail='Job not found')
+
+    if job.job_status == "error":
+        log.error(f'Job {job_id} errored before video creation')
+        raise HTTPException(status_code=404, detail='Job errored before video creation')
+
+    if job.job_status != "packed":
+        log.error(f'Job {job_id} video not packed')
+        raise HTTPException(status_code=404, detail='Video not created yet')
+
+    return FileResponse(job.video_urn)
