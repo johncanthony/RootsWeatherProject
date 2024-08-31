6615a38e5ffa93ee5089fc170be64016dc5ba136
johncanthony <john.claud.anthony@gmail.com>
2024-08-31 06:36:16 +0000

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RootsWeatherProject',
-    version='1.0.28',
+    version='1.0.29',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     packages=find_packages(),
