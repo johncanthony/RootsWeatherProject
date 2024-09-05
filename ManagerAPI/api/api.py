cea420a2adc760c5da45b24ea197293e982c0024
johncanthony <john.claud.anthony@gmail.com>
2024-09-05 04:23:58 +0000

    Added CORS middleware and testing origins for FE prototype to ManagerAPI

--- ManagerAPI/api/api.py
+++ ManagerAPI/api/api.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from ManagerAPI.api.routers import jobs, auth
 
 managerAPI = FastAPI()
 
+origins = [
+    "http://weather.memant.net",
+    "https://weather.memant.net",
+    "http://mac.memant.net",
+    "https://mac.memant.net"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @managerAPI.get('/health')
 async def health():
