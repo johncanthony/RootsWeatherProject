b5c4bc9b5f5db9fb84ebede6595593dd7feea1ad
johncanthony <john.claud.anthony@gmail.com>
2025-02-02 02:01:12 +0000

    Bumped package version for pypi build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RootsWeatherProject',
-    version='1.0.37',
+    version='1.0.38',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     packages=find_packages(),
