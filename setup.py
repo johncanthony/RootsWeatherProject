14bec3020e26704fa4c08651a1eca32e0d8de012
johncanthony <john.claud.anthony@gmail.com>
2025-02-12 22:23:09 +0000

    Bumped package version for gulf name change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RootsWeatherProject',
-    version='1.0.39',
+    version='1.0.40',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     packages=find_packages(),
